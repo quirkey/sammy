9e577460c2eb8ecc7a461578e02eaff1460bc7f2
Aaron Quint <aaron@quirkey.com>
2009-04-21 04:04:45 -0400

    FINALLY got some tests passing - needed to wrap blocks in document.ready

--- test/test_sammy_application.js
+++ test/test_sammy_application.js
@@ -1,135 +1,132 @@
 (function($) {
-
-  with(jqUnit) {
-
-    context('Sammy.Application', 'init', {
-      before: function() {
-        this.app = new Sammy.Application(function() {
-          this.random_setting = 1;
-        });
-      }
-    })
-    .should('create a sammy object', function() {
-      defined(this.a('app'), 'route');
-    })
-    .should('set arbitrary settings in the app', function() {
-      equals(this.a('app').random_setting, 1);
-    })
-    .should('initialize empty routes object', function() {
-      isType(this.a('app').routes, Object);
-    });
-
-
-    context('Sammy.Application', 'route', {
-      before: function() {
-        this.app = new Sammy.Application(function() {
-          this.route('get', /test/, function() {
-            $('#main').trigger('click');
+  $(function() {
+    with(jqUnit) {
+      context('Sammy.Application', 'init', {
+        before: function() {
+          this.app = new Sammy.Application(function() {
+            this.random_setting = 1;
           });
-
-          this.route('get', '/blah', function() {
-            $('#testarea').show();
-          });
-          
-          this.get(/blurgh/, function() {
-            alert('blurgh');
-          });
-        });
-      }
-    })
-    .should_eventually('throw error if parameters are not correct', function() {
-      var app = this.a('app');
-      raised(/route/, function () {
-        app.route('get', function() {});
+        }
+      })
+      .should('create a sammy object', function() {
+        defined(this.a('app'), 'route');
+      })
+      .should('set arbitrary settings in the app', function() {
+        equals(this.a('app').random_setting, 1);
+      })
+      .should('initialize empty routes object', function() {
+        isType(this.a('app').routes, Object);
       });
-    })		
-    .should_eventually('turn a string path into a regular expression', function() {
-      var app = this.a('app');
-      ok(app.routes['get']);
-      var route = app.routes['get'][1];
-      isType(route.path, RegExp);
-    })
-    .should('append route to application.routes object', function() {
-      var app = this.a('app');
-      ok(app.routes['get']);
-      var route = app.routes['get'][0]
-      isType(route.path, RegExp);
-      equals(route.verb, 'get');
-      defined(route, 'callback');
-    })
-    .should('allow shortcuts for defining routes', function() {
-      var app = this.a('app');
-      ok(app.routes['get']);
-      var route = app.routes['get'][2];
-      isType(route.path, RegExp);
-      equals(route.verb, 'get');
-      defined(route, 'callback');
-    });
-    //
-    
-    context('Sammy.Application','run', {
-      before: function () {
-        // $('.get_area').html('');
-        $('.get_area').show();
-        $('.get_area').text('test success');
-        this.app = new Sammy.Application(function() {
-          this.route('get', '#/test', function() {
-            $('#main').text('test success');
-            this.log('running bitches');
-          });
-        });
-        console.log(this.app);
-        this.app.run();
-      },
-      after: function () {
-        this.app.unload();
-      }
-    })
-    .should('attach application instance to body', function() {
-      isObj($.data('body', 'sammy.app'), this.a('app'));
-    })
-    .should_eventually('live bind events to all forms')
-    .should('trigger events on URL change', function() {
-      window.location = '#/test';
-      equals($('.get_area').text(), 'test success');
-    })
-    .should_eventually('bind event to clicks as specified by routes')
-    
-    context('Sammy.Application','lookupRoute', {
-      before: function() {
-        this.app = new Sammy.Application(function() {
-          this.route('get', /\/blah\/(.+)/, function() {
-            $('#main').trigger('click');
-          });
 
-          this.route('post', '/blah', function() {
-            $('#testarea').show();
+
+      context('Sammy.Application', 'route', {
+        before: function() {
+          this.app = new Sammy.Application(function() {
+            this.route('get', /test/, function() {
+              $('#main').trigger('click');
+            });
+
+            this.route('get', '/blah', function() {
+              $('#testarea').show();
+            });
+
+            this.get(/blurgh/, function() {
+              alert('blurgh');
+            });
           });
+        }
+      })
+      .should_eventually('throw error if parameters are not correct', function() {
+        var app = this.a('app');
+        raised(/route/, function () {
+          app.route('get', function() {});
         });
-      }
-    })
-    .should('find a route by verb and route', function() {
-      var app = this.a('app');
-      var route = app.lookupRoute('post','/blah');
-      isType(route, Object)
-      equals(route.verb, 'post');
-      defined(route, 'callback');
-    })
-    .should('find a route by verb and partial route', function() {
-      var app = this.a('app');
-      var route = app.lookupRoute('get','/blah/mess');
-      isType(route, Object)
-      equals(route.verb, 'get');
-      defined(route, 'callback');
-    })
-    .should('raise error when route can not be found', function() {
-      var app = this.a('app');
-      raised(/404/, function() {
-        app.lookupRoute('get','/blurgh');
+      })		
+      .should_eventually('turn a string path into a regular expression', function() {
+        var app = this.a('app');
+        ok(app.routes['get']);
+        var route = app.routes['get'][1];
+        isType(route.path, RegExp);
+      })
+      .should('append route to application.routes object', function() {
+        var app = this.a('app');
+        ok(app.routes['get']);
+        var route = app.routes['get'][0]
+        isType(route.path, RegExp);
+        equals(route.verb, 'get');
+        defined(route, 'callback');
+      })
+      .should('allow shortcuts for defining routes', function() {
+        var app = this.a('app');
+        ok(app.routes['get']);
+        var route = app.routes['get'][2];
+        isType(route.path, RegExp);
+        equals(route.verb, 'get');
+        defined(route, 'callback');
       });
-    })
-    .should_eventually('die silently if route is not found and 404s are off')
-    
-  }
+      //
 
+      context('Sammy.Application','run', {
+        before: function () {
+          $('.get_area').html('');
+          this.app = new Sammy.Application(function() {
+            this.route('get', '#/test', function() {
+              $('.get_area').text('test success');
+              this.log('running bitches');
+            });
+          });
+          this.app.run();
+        },
+        after: function () {
+          this.app.unload();
+        }
+      })
+      .should('attach application instance to body', function() {
+        console.log('app', this.a('app'), $.data('body', 'sammy.app'));
+        isObj($.data('body', 'sammy.app'), this.a('app'));
+      })
+      .should_eventually('live bind events to all forms')
+      .should('trigger events on URL change', function() {
+        window.location = '#/test';
+        equals($('.get_area').text(), 'test success');
+      })
+      .should_eventually('bind event to clicks as specified by routes')
+
+      context('Sammy.Application','lookupRoute', {
+        before: function() {
+          this.app = new Sammy.Application(function() {
+            this.route('get', /\/blah\/(.+)/, function() {
+              $('#main').trigger('click');
+            });
+
+            this.route('post', '/blah', function() {
+              $('#testarea').show();
+            });
+          });
+        }
+      })
+      .should('find a route by verb and route', function() {
+        var app = this.a('app');
+        var route = app.lookupRoute('post','/blah');
+        isType(route, Object)
+        equals(route.verb, 'post');
+        defined(route, 'callback');
+      })
+      .should('find a route by verb and partial route', function() {
+        var app = this.a('app');
+        var route = app.lookupRoute('get','/blah/mess');
+        isType(route, Object)
+        equals(route.verb, 'get');
+        defined(route, 'callback');
+      })
+      .should('raise error when route can not be found', function() {
+        var app = this.a('app');
+        raised(/404/, function() {
+          app.lookupRoute('get','/blurgh');
+        });
+      })
+      .should_eventually('die silently if route is not found and 404s are off')
+
+    }
+  });
 })(jQuery);
--- test/test_sammy_event_context.js
+++ test/test_sammy_event_context.js
@@ -1,59 +1,64 @@
 (function($) {
-  
-  with(jqUnit) {
-    var test_app = new Sammy.Application(function() {});
-    var test_context = new Sammy.EventContext(test_app, 'get', '#/test/:test', {test: 'hooray'});
-    
-    context('Sammy', 'EventContext','init', {
-      before: function() {
-        this.app = test_app;
-        this.context = test_context;
-      }
-    })
-    .should('set app', function() {
-      isObj(this.a('context').app, this.a('app'));
-    })
-    .should('set verb', function() {
-      equals(this.a('context').verb, 'get');
-    })
-    .should('set path', function() {
-      equals(this.a('context').path, '#/test/:test');
-    })
-    .should('set params', function() {
-      isObj(this.a('context').params, {test: 'hooray'});
-    });
-    
-    context('Sammy', 'EventContext', 'redirect')
-    .should_eventually('set full location if url is provided')
-    .should_eventually('only set hash if location is prefixed with #');
-    
-    context('Sammy', 'EventContext', 'raise')
-    .should_eventually('throw error');
-    
-    context('Sammy', 'EventContext', 'not_found')
-    .should_eventually('throw not found error')
-    
-    context('Sammy', 'EventContext', 'render', 'text', {
-      before: function() {
-        this.context = test_context;
-        this.context.render('text', '#test_area', 'test it')
-      }
-    })
-    .should('put text in selector', function() {
-      equals($('#test_area').text(), 'test it');
-    });
-    
-    context('Sammy', 'EventContext', 'render', 'html', {
-      before: function() {
-        this.context = test_context;
-        this.context.render('html', '#test_area', '<div class="test_class">TEST!</div>')
-      }
-    })
-    .should('put html in selector', function() {
-      equals($('#test_area').html(), '<div class="test_class">TEST!</div>');
-    });
-    
-    
-  };
-  
+  $(function() {
+    with(jqUnit) {
+      var test_app = new Sammy.Application(function() {});
+      var test_context = new Sammy.EventContext(test_app, 'get', '#/test/:test', {test: 'hooray'});
+
+      context('Sammy', 'EventContext','init', {
+        before: function() {
+          this.app = test_app;
+          this.context = test_context;
+        }
+      })
+      .should('set app', function() {
+        isObj(this.a('context').app, this.a('app'));
+      })
+      .should('set verb', function() {
+        equals(this.a('context').verb, 'get');
+      })
+      .should('set path', function() {
+        equals(this.a('context').path, '#/test/:test');
+      })
+      .should('set params', function() {
+        isObj(this.a('context').params, {test: 'hooray'});
+      });
+
+      context('Sammy', 'EventContext', 'redirect', {
+        before: function() {
+          this.app = test_app;
+          this.context = test_context;
+        }
+      })
+      .should('set full location if url is provided')
+      .should_eventually('only set hash if location is prefixed with #');
+
+      context('Sammy', 'EventContext', 'raise')
+      .should_eventually('throw error');
+
+      context('Sammy', 'EventContext', 'not_found')
+      .should_eventually('throw not found error')
+
+      context('Sammy', 'EventContext', 'render', 'text', {
+        before: function() {
+          this.context = test_context;
+          this.context.render('text', '#test_area', 'test it')
+        }
+      })
+      .should('put text in selector', function() {
+        equals($('#test_area').text(), 'test it');
+      });
+
+      context('Sammy', 'EventContext', 'render', 'html', {
+        before: function() {
+          this.context = test_context;
+          this.context.render('html', '#test_area', '<div class="test_class">TEST!</div>')
+        }
+      })
+      .should('put html in selector', function() {
+        equals($('#test_area').html(), '<div class="test_class">TEST!</div>');
+      });
+
+
+    };
+  });
 })(jQuery);
