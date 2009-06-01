dafd03b40fe78afbae006b691e45965356533239
Aaron Quint <aaron@quirkey.com>
2009-06-01 14:51:23 -0400

    New: helpers() allows you to extend the applications EventContext object with helper functions
    Tests added for helpers.
    Fixed: Added toString() for error checks to ensure we can .match()
    Bumped to version 0.1.3

--- test/test_sammy_application.js
+++ test/test_sammy_application.js
@@ -332,7 +332,6 @@
         }, this, 1, 2);
       });      
       
-      
       context('Sammy.Application','after', {
         before: function() {
           var context = this;
@@ -368,6 +367,50 @@
         });
       });      
       
+      
+      context('Sammy.Application','helpers', {
+        before: function() {
+          var context = this;
+          context.event_context = null;
+          this.app = new Sammy.Application(function() {
+            
+            this.helpers({
+              helpme: function() {
+                return "halp!";
+              }
+            });
+            
+            this.get('#/', function() {
+              this.params['belch'] = 'boosh';
+              context.event_context = this;
+            });
+            
+            this.bind('blurgh', function() {
+              context.event_context = this;
+            });
+          });
+        }
+      })
+      .should('extend event context for routes', function() {
+        var context = this;
+        this.app.run('#/');
+        soon(function() {
+          ok(context['event_context']);
+          isType(context.event_context.helpme, Function);
+          this.app.unload();
+        }, this, 2, 2);
+      })
+      .should('extend event context for bind', function() {
+        var context = this;
+        this.app.run('#/');
+        this.app.trigger('blurgh');
+        soon(function() {
+          ok(context['event_context']);
+          isType(context.event_context.helpme, Function);
+          this.app.unload();
+        }, this, 2, 2);
+      });
+            
     }
   // });
 })(jQuery);
