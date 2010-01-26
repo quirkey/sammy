58b2650ef0df7b0adb8e83d80625fc531812714a
Aaron Quint <aaron@quirkey.com>
2010-01-25 23:40:59 -0500

    Catch errors for Sammy.Store.isAvailable();

--- test/test_sammy_storage.js
+++ test/test_sammy_storage.js
@@ -4,156 +4,158 @@
     var stores = ['memory', 'data', 'local', 'session', 'cookie'];
 
     $.each(stores, function(i, store_type) {
-      context('Sammy.Store', store_type, {
-        before: function() {
-          this.store_attributes = {
-            element: '#main',
-            name: 'test_store',
-            type: store_type
-          };
-          this.store = new Sammy.Store(this.store_attributes);
+      if (Sammy.Store.isAvailable(store_type)) {
+        context('Sammy.Store', store_type, {
+          before: function() {
+            this.store_attributes = {
+              element: '#main',
+              name: 'test_store',
+              type: store_type
+            };
+            this.store = new Sammy.Store(this.store_attributes);
 
-          this.other_store = new Sammy.Store({
-            element: '#main',
-            name: 'other_test_store',
-            type: store_type
-          });
+            this.other_store = new Sammy.Store({
+              element: '#main',
+              name: 'other_test_store',
+              type: store_type
+            });
+            this.store.clearAll();
+            this.other_store.clearAll();
+          }
+        })
+        .should('set store type', function() {
+          equals(this.store.type, store_type);
+        })
+        .should('set name', function() {
+          equals(this.store.name, 'test_store');
+        })
+        .should('set the element', function() {
+          equals(this.store.$element[0], $('#main')[0]);
+        })
+        .should('check if a key exists', function() {
+          ok(!this.store.exists('foo'));
+          this.store.set('foo', 'bar');
+          ok(this.store.exists('foo'));
+          ok(!this.other_store.exists('foo'));
+        })
+        .should('set and retrieve value as string', function() {
+          ok(this.store.set('foo', 'bar'));
+          equals(this.store.get('foo'), 'bar');
+          ok(!this.other_store.get('foo'));
+        })
+        .should('set and retrieve value as JSON', function() {
+          ok(this.store.set('foo', {'obj': 'is json'}));
+          equals(this.store.get('foo').obj,'is json');
+          ok(!this.other_store.get('foo'));
+        })
+        .should('should store in global space accessible by name', function() {
+          this.store.set('foo', 'bar');
+          var new_store = new Sammy.Store(this.store_attributes);
+          equals(new_store.get('foo'), 'bar');
+        })
+        .should('clear value', function() {
+          ok(this.store.set('foo', 'bar'));
+          ok(this.other_store.set('foo', 'bar'));
+          equals(this.store.get('foo'), 'bar');
+          this.store.clear('foo');
+          ok(!this.store.exists('foo'));
+          ok(this.other_store.get('foo'), 'bar');
+        })
+        .should('return list of keys', function() {
+          this.store.set('foo', 'bar');
+          this.store.set('blurgh', {boosh: 'blurgh'});
+          this.store.set(123, {boosh: 'blurgh'});
+          isObj(this.store.keys(), ['foo', 'blurgh', '123'], "keys were " + this.store.keys().toString());
+          isObj(this.other_store.keys(), []);
+        })
+        .should('clear all values', function() {
+          this.store.set('foo', 'bar');
+          this.store.set('blurgh', {boosh: 'blurgh'});
+          this.store.set(123, {boosh: 'blurgh'});
+          equals(this.store.keys().length, 3);
           this.store.clearAll();
-          this.other_store.clearAll();
+          equals(this.store.keys().length, 0);
+          ok(!this.store.exists('blurgh'));
+        })
+        .should('fire events on get and set', function() {
+          var fired = false;
+          $('#main').bind('set-test_store.foo', function(e, key, value) {
+            fired = value;
+          });
+          this.store.set('foo', 'bar');
+          soon(function() {
+            equals(fired, 'bar');
+            $('#main').unbind('set-test_store.foo');
+          });
+        }) 
+        .should('fetch value or run callback', function() {
+          ok(!this.store.get('foo'));
+          this.store.fetch('foo', function() {
+            return "bar";
+          });
+          equals(this.store.get('foo'), 'bar');
+          equals(this.store.fetch('foo', function() {
+            return "baz";
+          }), 'bar');
+          equals(this.store.get('foo'), 'bar');
+        });
+      }
+    });
+  
+      context('Sammy.Storage', {
+        before: function() {
+          this.app = new Sammy.Application(function() {
+            this.use(Sammy.Storage);
+          });
+          this.context = new this.app.context_prototype(this.app, 'get', '#/', {});
         }
       })
-      .should('set store type', function() {
-        equals(this.store.type, store_type);
+      .should('add the store method to the app', function() {
+        ok($.isFunction(this.app.store));
       })
-      .should('set name', function() {
-        equals(this.store.name, 'test_store');
+      .should('add the store method to event contexts', function() {
+        ok($.isFunction(this.context.store));
+      });
+    
+      context('Sammy.Storage', 'store', {
+        before: function() {
+          var store = null;
+          this.app = new Sammy.Application(function() {
+            this.use(Sammy.Storage);
+            store = this.store('session');
+          });
+          this.context = new this.app.context_prototype(this.app, 'get', '#/', {});
+          store.clearAll();
+          this.store = store;
+        }
       })
-      .should('set the element', function() {
-        equals(this.store.$element[0], $('#main')[0]);
+      .should('create a new sammy store if it doesnt exist', function() {
+        ok(this.store);
+        ok($.isFunction(this.store.get));
       })
-      .should('check if a key exists', function() {
-        ok(!this.store.exists('foo'));
-        this.store.set('foo', 'bar');
-        ok(this.store.exists('foo'));
-        ok(!this.other_store.exists('foo'));
+      .should('add the a named method shortcut to the app', function() {
+        ok($.isFunction(this.app.session));
       })
-      .should('set and retrieve value as string', function() {
-        ok(this.store.set('foo', 'bar'));
+      .should('add a named method shortcut to the event contexts', function() {
+        ok($.isFunction(this.context.session));
+      })
+      .should('should set value if value is passed', function() {
+        this.context.session('foo', 'bar')
         equals(this.store.get('foo'), 'bar');
-        ok(!this.other_store.get('foo'));
       })
-      .should('set and retrieve value as JSON', function() {
-        ok(this.store.set('foo', {'obj': 'is json'}));
-        equals(this.store.get('foo').obj,'is json');
-        ok(!this.other_store.get('foo'));
-      })
-      .should('should store in global space accessible by name', function() {
+      .should('should get value if no value is passed', function() {
         this.store.set('foo', 'bar');
-        var new_store = new Sammy.Store(this.store_attributes);
-        equals(new_store.get('foo'), 'bar');
+        equals(this.context.session('foo'), 'bar');
       })
-      .should('clear value', function() {
-        ok(this.store.set('foo', 'bar'));
-        ok(this.other_store.set('foo', 'bar'));
-        equals(this.store.get('foo'), 'bar');
-        this.store.clear('foo');
-        ok(!this.store.exists('foo'));
-        ok(this.other_store.get('foo'), 'bar');
-      })
-      .should('return list of keys', function() {
-        this.store.set('foo', 'bar');
-        this.store.set('blurgh', {boosh: 'blurgh'});
-        this.store.set(123, {boosh: 'blurgh'});
-        isObj(this.store.keys(), ['foo', 'blurgh', '123'], "keys were " + this.store.keys().toString());
-        isObj(this.other_store.keys(), []);
-      })
-      .should('clear all values', function() {
-        this.store.set('foo', 'bar');
-        this.store.set('blurgh', {boosh: 'blurgh'});
-        this.store.set(123, {boosh: 'blurgh'});
-        equals(this.store.keys().length, 3);
-        this.store.clearAll();
-        equals(this.store.keys().length, 0);
-        ok(!this.store.exists('blurgh'));
-      })
-      .should('fire events on get and set', function() {
-        var fired = false;
-        $('#main').bind('set-test_store.foo', function(e, key, value) {
-          fired = value;
-        });
-        this.store.set('foo', 'bar');
-        soon(function() {
-          equals(fired, 'bar');
-          $('#main').unbind('set-test_store.foo');
-        });
-      }) 
-      .should('fetch value or run callback', function() {
+      .should('call fetch if callback is passed', function() {
         ok(!this.store.get('foo'));
-        this.store.fetch('foo', function() {
+        this.context.session('foo', function() {
           return "bar";
         });
         equals(this.store.get('foo'), 'bar');
-        equals(this.store.fetch('foo', function() {
+        equals(this.context.session('foo', function() {
           return "baz";
         }), 'bar');
-        equals(this.store.get('foo'), 'bar');
-      })
-    });
-  
-    context('Sammy.Storage', {
-      before: function() {
-        this.app = new Sammy.Application(function() {
-          this.use(Sammy.Storage);
-        });
-        this.context = new this.app.context_prototype(this.app, 'get', '#/', {});
-      }
-    })
-    .should('add the store method to the app', function() {
-      ok($.isFunction(this.app.store));
-    })
-    .should('add the store method to event contexts', function() {
-      ok($.isFunction(this.context.store));
-    });
-    
-    context('Sammy.Storage', 'store', {
-      before: function() {
-        var store = null;
-        this.app = new Sammy.Application(function() {
-          this.use(Sammy.Storage);
-          store = this.store('session');
-        });
-        this.context = new this.app.context_prototype(this.app, 'get', '#/', {});
-        store.clearAll();
-        this.store = store;
-      }
-    })
-    .should('create a new sammy store if it doesnt exist', function() {
-      ok(this.store);
-      ok($.isFunction(this.store.get));
-    })
-    .should('add the a named method shortcut to the app', function() {
-      ok($.isFunction(this.app.session));
-    })
-    .should('add a named method shortcut to the event contexts', function() {
-      ok($.isFunction(this.context.session));
-    })
-    .should('should set value if value is passed', function() {
-      this.context.session('foo', 'bar')
-      equals(this.store.get('foo'), 'bar');
-    })
-    .should('should get value if no value is passed', function() {
-      this.store.set('foo', 'bar');
-      equals(this.context.session('foo'), 'bar');
-    })
-    .should('call fetch if callback is passed', function() {
-      ok(!this.store.get('foo'));
-      this.context.session('foo', function() {
-        return "bar";
-      });
-      equals(this.store.get('foo'), 'bar');
-      equals(this.context.session('foo', function() {
-        return "baz";
-      }), 'bar');
     });
   };
 })(jQuery);
