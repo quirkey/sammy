c3ee7f2e14091d74326b39e5935447e9109408c2
Aaron Quint <aaron@quirkey.com>
2010-04-02 13:28:57 -0400

    Updated Site via Rake

--- test/test/test_sammy_plugins.js
+++ test/test/test_sammy_plugins.js
@@ -105,9 +105,20 @@
          $('#nested_params_test_form').submit();
          soon(function() {
            ok(app.form_params);
-           equal(app.form_params['author'], 'Thoreau');        
+           equal(app.form_params['author'], 'Thoreau', 'takes the last value');        
            app.unload();
          }, this, 1, 2);
+       })
+       .should('parse true and false values as true and false literals', function() {
+         var app = this.app;
+         app.run('#/');
+         $('#nested_params_test_form').submit();
+         soon(function() {
+           ok(app.form_params);
+           equal(app.form_params['falsevalue'], false);
+           equal(app.form_params['truevalue'], true);
+           app.unload();
+         }, this, 1, 3);
        })
        .should('parse basic arrays', function() {
          var app = this.app;
@@ -289,7 +300,115 @@
       })
       .should('use haml-js to render haml templates', function() {
         var template = ".mytemplate= title";
-        deepEqual(this.context.haml(template, {title: "HAML!!"}), "<div class=\"mytemplate\">HAML!!\n</div>");
+        deepEqual(this.context.haml(template, {title: "HAML!!"}), "<div class=\"mytemplate\">HAML!!</div>");
+      });
+      
+      context('Sammy', 'Form', {
+        before: function() {
+          this.app = new Sammy.Application(function() {
+            this.use(Sammy.Form);
+          });
+          this.context = new this.app.context_prototype(this.app, 'get', '#/', {});
+          this.item = {
+            name: 'Test Item'
+          };
+        }
+      })
+      .should("return simple string element with simple_element", function() {
+        equal(this.context.simple_element('div', {'class': 'test'}, "test"), "<div class='test'>test</div>");
+      })
+      .should("create self closing if no content is passed", function() {
+        equal(this.context.simple_element('div', {'class': 'test'}), "<div class='test' />");
+      })
+      .should("evaluate attributes that are functions", function() {
+        equal(this.context.simple_element('div', {id: function() { return 'test'; }}), "<div id='test' />");
+      });
+      
+      context('Sammy', 'Form', 'FormBuilder', {
+        before: function() {
+          this.app = new Sammy.Application(function() {
+            this.use(Sammy.Form);
+            this.use(Sammy.Template);
+          });
+          this.context = new this.app.context_prototype(this.app, 'get', '#/', {});
+          
+          // test item
+          this.item = {
+            id: "1234",
+            name: 'Item Name',
+            price: '$10.00',
+            quantity: 5,
+            description: "This is a long\ndescription",
+            color: function() {
+              return 'red';
+            },
+            meta: {
+              url: 'http://www.quirkey.com'
+            },
+            is_private: false,
+            related: [
+              {name: 'Related 1'},
+              {name: 'Related 2'}
+            ]
+          };
+          this.builder = new Sammy.FormBuilder('item', this.item);
+        }
+      })
+      .should("create form builder with name and object", function() {
+        ok(this.builder);
+        equal(this.builder.name, 'item');
+        deepEqual(this.builder.object, this.item);
+      })
+      .should("return text field for attribute with simple keypath", function() {
+        equal(this.builder.text('name'), "<input type='text' name='item[name]' value='Item Name' class='item-name' />")
+      })
+      .should("return text field with additional attributes", function() {
+        equal(this.builder.text('name', {rel: 'test'}), "<input type='text' name='item[name]' value='Item Name' class='item-name' rel='test' />")
+      })
+      .should("return text field when the attribute doesnt exist", function() {
+        equal(this.builder.text('none'), "<input type='text' name='item[none]' value='' class='item-none' />")
+      })
+      .should("return text field for an attribute with a deep keypath", function() {
+        equal(this.builder.text('meta.url'), "<input type='text' name='item[meta][url]' value='http://www.quirkey.com' class='item-meta-url' />")
+        equal(this.builder.text(['meta', 'url']), "<input type='text' name='item[meta][url]' value='http://www.quirkey.com' class='item-meta-url' />")
+      })
+      .should("return text field for an attribute with an array keypath", function() {
+        equal(this.builder.text('related.0.name'), "<input type='text' name='item[related][0][name]' value='Related 1' class='item-related-0-name' />")
+      })
+      .should("return a select tag with options and selection", function() {
+        equal(this.builder.select('color', ['blue', 'red', 'green']), "<select name='item[color]' class='item-color'><option value='blue'>blue</option><option value='red' selected='selected'>red</option><option value='green'>green</option></select>")
+      })
+      .should("return a label with key as for", function() {
+        equal(this.builder.label('name', 'Name'), "<label for='item[name]'>Name</label>");
+      })
+      .should("return a hidden input", function() {
+        equal(this.builder.hidden('id'), "<input type='hidden' name='item[id]' value='1234' class='item-id' />");
+      })
+      .should("return a textarea", function() {
+        equal(this.builder.textarea('description'), "<textarea name='item[description]' class='item-description'>This is a long\ndescription</textarea>");
+      })
+      .should("return a checkbox", function() {
+        equal(this.builder.checkbox('is_private', true), "<input type='hidden' name='item[is_private]' value='false' class='item-is_private' /><input type='checkbox' name='item[is_private]' value='true' class='item-is_private' />");
+        this.item.is_private = true;
+        equal(this.builder.checkbox('is_private', true), "<input type='hidden' name='item[is_private]' value='false' class='item-is_private' /><input type='checkbox' name='item[is_private]' value='true' class='item-is_private' checked='checked' />")
+      })
+       .should("return a checkbox with no hidden element", function() {
+          equal(this.builder.checkbox('is_private', true, {hidden_element: false}), "<input type='checkbox' name='item[is_private]' value='true' class='item-is_private' />");
+          this.item.is_private = true;
+          equal(this.builder.checkbox('is_private', true, {hidden_element: false}), "<input type='checkbox' name='item[is_private]' value='true' class='item-is_private' checked='checked' />")
+        })
+      .should("return a radio button", function() {
+        equal(this.builder.radio('quantity', 5), "<input type='radio' name='item[quantity]' value='5' class='item-quantity' checked='checked' />");
+      })
+      .should("build a form with form in a template", function() {
+        var template = "<% formFor('item', function(f) { %>" +
+                       "<%= f.open() %>" +
+                       "<p><label>Name:</label><%= f.text('name') %></p>" +
+                       "<%= f.close() %>" +
+                       "<% }); %>";
+        var rendered = "<form method='post' action='#/items'><p><label>Name:</label><input type='text' name='item[name]' value='Item Name' class='item-name' /></p></form>"
+        this.context.item = this.item;
+        equals(this.context.template(template), rendered);
       });
       
     };
