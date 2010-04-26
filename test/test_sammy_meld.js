34e6793cbb59a4a5231f19a3a9ceebade5b2f9de
Aaron Quint <aaron@quirkey.com>
2010-04-26 11:44:43 -0400

    Starting work on importing/rewriting the meld template framework

---
+++ test/test_sammy_meld.js
@@ -0,0 +1,31 @@
+(function($) {
+    with(QUnit) {
+            
+      context('Sammy', 'Meld', {
+          before: function() {
+            var test_app = new Sammy.Application(function() {
+              this.raise_errors = false;
+              this.element_selector = '#main';
+              this.use(Sammy.Meld);
+            });
+            this.test_context = new test_app.context_prototype(test_app, 'get', '#/test/:test', {test: 'hooray'});
+            $('#test_area').html('');
+          }
+        })
+        .should('do simple interpolation', function() {
+          var template = "<div class='title'></div>";
+              data = {'title': 'TEST'},
+              expected = "<div class='title'>TEST</div>";
+          equal(this.test_context.meld(template, data), expected);
+        })
+        .should('interpolate one level deep', function() {
+          var template = "<div class='post'><div class='title'></div></div>";
+              data = {'post': {'title': 'TEST'}},
+              expected = "<div class='post'><div class='title'>TEST</div></div>";
+          equal(this.test_context.meld(template, data), expected);
+        });
+
+      
+      
+    };
+})(jQuery);
