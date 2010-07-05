bc8f621c42d0d9dfa008a42493d44b58e4565242
Aaron Quint <aaron@quirkey.com>
2010-07-04 23:21:58 -0700

    Basic working meld tests

--- test/test_sammy_meld.js
+++ test/test_sammy_meld.js
@@ -1,6 +1,10 @@
 (function($) {
     with(QUnit) {
-            
+      
+      function sameHTML(actual, expected) {
+        equal($(actual)[0].outerHTML, $(expected)[0].outerHTML);
+      };
+      
       context('Sammy', 'Meld', {
           before: function() {
             var test_app = new Sammy.Application(function() {
@@ -9,20 +13,20 @@
               this.use(Sammy.Meld);
             });
             this.test_context = new test_app.context_prototype(test_app, 'get', '#/test/:test', {test: 'hooray'});
-            $('#test_area').html('');
+            // $('#test_area').html('');
           }
         })
         .should('do simple interpolation', function() {
           var template = "<div class='title'></div>";
               data = {'title': 'TEST'},
               expected = "<div class='title'>TEST</div>";
-          equal(this.test_context.meld(template, data), expected);
+          sameHTML(this.test_context.meld(template, data), expected);
         })
         .should('interpolate one level deep', function() {
           var template = "<div class='post'><div class='title'></div></div>";
               data = {'post': {'title': 'TEST'}},
               expected = "<div class='post'><div class='title'>TEST</div></div>";
-          equal(this.test_context.meld(template, data), expected);
+          sameHTML(this.test_context.meld(template, data), expected);
         });
 
       
