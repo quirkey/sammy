7e18245f12a1908a631f5918ef147f9c28b06bf9
Aaron Quint <aaron@quirkey.com>
2010-07-04 23:47:39 -0700

    Meld handles arrays

--- test/test_sammy_meld.js
+++ test/test_sammy_meld.js
@@ -27,8 +27,38 @@
               data = {'post': {'title': 'TEST'}},
               expected = "<div class='post'><div class='title'>TEST</div></div>";
           sameHTML(this.test_context.meld(template, data), expected);
-        });
-
+        })
+        .should('interpolate multiple keys', function() {
+          var template = "<div class='post'></div><div class='title'></div>";
+              data = {'post': 'my post', 'title': 'TEST'},
+              expected = "<div class='post'>my post</div><div class='title'>TEST</div>";
+          sameHTML(this.test_context.meld(template, data), expected);
+        })
+        .should('interpolate multiple nested keys', function() {
+          var template = "<div class='post'><div class='title'></div><div class='author'></div></div>";
+              data = {'post': {'title': 'TEST', 'author': 'AQ'}},
+              expected = "<div class='post'><div class='title'>TEST</div><div class='author'>AQ</div></div>";
+          sameHTML(this.test_context.meld(template, data), expected);
+        })
+        .should('not interpolate incorrectly nested keys', function() {
+          var template = "<div class='post'></div><div class='title'></div>";
+              data = {'post': {'title': 'TEST'}},
+              expected = "<div class='post'></div><div class='title'></div>";
+          sameHTML(this.test_context.meld(template, data), expected);
+        })
+        .should('multiply an array of tags', function() {
+          var template = "<div class='post'><span class='tags'></span></div>";
+              data = {'post': {'tags': ['one', 'two']}},
+              expected = "<div class='post'><span class='tags'>one</span><span class='tags'>two</span></div>";
+          sameHTML(this.test_context.meld(template, data), expected);
+        })
+        .should('multiply an array of objects', function() {
+          var template = "<div class='post'><div class='authors'><h2 class='name'></h2><span class='twitter'></span></div></div>";
+              data = {'post': {'authors': [{'name': 'AQ', 'twitter': 'aq'}, {'name':'Mike', 'twitter':'mrb_bk'}]}},
+              expected = "<div class='post'><div class='authors'><h2 class='name'>AQ</h2><span class='twitter'>aq</span></div><div class='authors'><h2 class='name'>Mike</h2><span class='twitter'>mrb_bk</span></div></div>";
+          sameHTML(this.test_context.meld(template, data), expected);
+        })
+        ;
       
       
     };
